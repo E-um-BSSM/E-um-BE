c77040f5ac82123468dd2a7e3e859a336217028e
Kr-Verified <dmschd0573@gmail.com>
2025-09-04 19:08:08 +0900

    add entity and repository

---
+++ src/main/java/eum/eum/entity/User.java
@@ -0,0 +1,30 @@
+package eum.eum.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password; // BCrypt로 암호화 저장
+    private Integer strength = 1000;
+    private String systemRole; // student | admin
+    private String bsmId;
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ src/main/java/eum/eum/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package eum.eum.repository;
+
+import eum.eum.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
