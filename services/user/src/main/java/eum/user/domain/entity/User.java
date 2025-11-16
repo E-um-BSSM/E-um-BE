cc37a2f6b3bc5f054ae63de602ca3046c4b9d5a0
Kr-Verified <dmschd0573@gmail.com>
2025-11-17 08:39:48 +0900

    fix: id bug

--- services/user/src/main/java/eum/user/domain/entity/User.java
+++ services/user/src/main/java/eum/user/domain/entity/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UuidGenerator;
-import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
